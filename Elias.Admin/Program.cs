f21e6558dec1ee168c09199255aed1ddb3678811
EliAs <EliAs@DESKTOP-K95LG4P>
2023-12-19 14:27:36 +0330

    config Program Of Admin

--- Elias.Admin/Program.cs
+++ Elias.Admin/Program.cs
@@ -1,9 +1,17 @@
+using Elias.Data.Context;
+
 var builder = WebApplication.CreateBuilder(args);
+#region Services
+var services = builder.Services;
+services.AddRazorPages();
 
+services.AddDbContext<DatabaseContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("EliasConnectionString")));
+#endregion
 // Add services to the container.
-builder.Services.AddRazorPages();
 
+#region Middlewares
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -23,3 +31,5 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
+#endregion
+
