9eb72642552b13a064755a5c91220cd8a54530c9
elias.merati <elias.merati@gmail.com>
2024-03-10 00:10:21 +0330

    Update skill

--- Elias.Admin/Pages/Skill/UpdateSkill.cshtml.cs
+++ Elias.Admin/Pages/Skill/UpdateSkill.cshtml.cs
@@ -1,12 +1,40 @@
+using Elias.Application.Interfaces;
+using Elias.Data.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Elias.Admin.Pages.Skill
 {
     public class UpdateSkillModel : PageModel
     {
-        public void OnGet()
+        #region Inject Service
+        private readonly ISkillService _skillService;
+        private readonly ISkillGroupService _skillGroupService;
+        public UpdateSkillModel(ISkillService skillService, ISkillGroupService skillGroupService)
         {
+            _skillService = skillService;
+            _skillGroupService = skillGroupService;
+        }
+        #endregion
+        [BindProperty]
+        public Elias.Data.Entities.Skills.Skill Skill { get; set; }
+        public async Task<IActionResult> OnGet(int Id)
+        {
+            var groups = _skillGroupService.GetSkillGroup();
+            ViewData["Groups"] = new SelectList(groups, "Value", "Text");
+
+            Skill = await _skillService.FindSkillById(Id);
+            if (Skill == null)
+                return NotFound();
+            return Page();
+            
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            await _skillService.UpdateSkill(Skill);
+            return RedirectToPage("Index");
         }
     }
 }
