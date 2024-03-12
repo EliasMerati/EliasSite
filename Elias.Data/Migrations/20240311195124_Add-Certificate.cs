266d992ed72bea63f067be89f661f8446b103a63
elias.merati <elias.merati@gmail.com>
2024-03-12 13:26:11 +0330

    Add Certificate Services

---
+++ Elias.Data/Migrations/20240311195124_Add-Certificate.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Elias.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCertificate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Certificates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CertificateName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CertificateDate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CertificateID = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CertificateImage = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Certificates", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreateDate",
+                value: new DateTime(2024, 3, 11, 23, 21, 22, 693, DateTimeKind.Local).AddTicks(1565));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Certificates");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreateDate",
+                value: new DateTime(2024, 3, 10, 21, 47, 0, 240, DateTimeKind.Local).AddTicks(6537));
+        }
+    }
+}
