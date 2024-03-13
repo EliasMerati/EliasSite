d7d621cadfecfc9ad9587d83f7b08c65e0439608
elias.merati <elias.merati@gmail.com>
2024-03-13 23:22:11 +0330

    create PortfolioGroup Entity

---
+++ Elias.Data/Entities/Portfolio/PortfolioGroup.cs
@@ -0,0 +1,9 @@
+﻿using Elias.Data.Common;
+
+namespace Elias.Data.Entities.Portfolio
+{
+    public class PortfolioGroup : BaseEntity<int>
+    {
+        public string GroupName { get; set; }
+    }
+}
